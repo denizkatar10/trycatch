c0bd6951f100bc1336875ce7e6a86de194d9c027
Deniz Katar <dnzkatar@gmail.com>
2023-03-25 02:43:16 -0400

    new

---
+++ src/TryCatchExamples.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TryCatchExamples {
+
+	public static void main(String[] args) {
+		
+		
+		
+		try {
+			int[] a = {4};
+			System.out.println(a[1]);
+		}catch(NullPointerException e) {
+			System.out.println("Your array us null!");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Your index is out of bounds!");
+		}catch(Exception e){
+			System.out.println("Something else went wrong!");
+		}
+
+	}
+
+}
